c3d4856fde137baf7f09db3c93dea82a6ef53ad8
Adel Absalyamov <adel.absalyamov@yandex.ru>
2021-10-09 14:16:40 +0300

    preloader add

--- src/components/main/mainContsainer.jsx
+++ src/components/main/mainContsainer.jsx
@@ -2,43 +2,47 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./mainContainer.css";
+import Preloader from "../../utils/preloader";
 
 const MainContainer = ({ users }) => {
-  console.log(users);
-  return (
-    <>
-      <div className="main-text">
-        <p>Text....</p>
-      </div>
-      <div className="container">
-        <div className="row">
-          {users &&
-            users.map((item) => {
-              return (
-                <div className="col-6 col-sm-3 mt-3" key={item.id}>
-                  <div className="card" style={{ width: 18 + "rem" }}>
-                    <img
-                      src={item.src}
-                      alt="картинка"
-                      className="card-img-top"
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{item.name}</h5>
-                      <p className="card-text">{`${item.age} года`}</p>
-                      <p className="about">{item.aboutMe}</p>
-                      <div className="link-btn-card">
-                        <Link to="#">Открыть</Link>
-                        <Link to="#">В избранное</Link>
+  if (users) {
+    return (
+      <>
+        <div className="main-text">
+          <p>Text....</p>
+        </div>
+        <div className="container">
+          <div className="row">
+            {users &&
+              users.map((item) => {
+                return (
+                  <div className="col-6 col-sm-3 mt-3" key={item.id}>
+                    <div className="card" style={{ width: 18 + "rem" }}>
+                      <img
+                        src={item.src}
+                        alt="картинка"
+                        className="card-img-top"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">{item.name}</h5>
+                        <p className="card-text">{`${item.age} года`}</p>
+                        <p className="about">{item.aboutMe}</p>
+                        <div className="link-btn-card">
+                          <Link to="#">Открыть</Link>
+                          <Link to="#">В избранное</Link>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return <Preloader />;
+  }
 };
 MainContainer.propTypes = {
   users: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
