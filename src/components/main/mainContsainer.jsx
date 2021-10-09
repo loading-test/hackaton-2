8692d2bdcfe28115789114ac62d2e1d9d3a69c31
Adel Absalyamov <adel.absalyamov@yandex.ru>
2021-10-09 14:00:50 +0300

    mainContainer

--- src/components/main/mainContsainer.jsx
+++ src/components/main/mainContsainer.jsx
@@ -1,30 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./mainContainer.css";
 
 const MainContainer = ({ users }) => {
   console.log(users);
   return (
-    <div className="container">
-      <div>
+    <>
+      <div className="main-text">
         <p>Text....</p>
       </div>
-      <div className="container mt-5">
+      <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card" style={{ width: 18 + "rem" }}>
-              <img src="#" alt="картинка" className="card-img-top" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">card</p>
-                <Link to="#">Открыть</Link>
-                <Link to="#">Добавить в избранное</Link>
-              </div>
-            </div>
-          </div>
+          {users &&
+            users.map((item) => {
+              return (
+                <div className="col-6 col-sm-3 mt-3" key={item.id}>
+                  <div className="card" style={{ width: 18 + "rem" }}>
+                    <img
+                      src={item.src}
+                      alt="картинка"
+                      className="card-img-top"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <p className="card-text">{`${item.age} года`}</p>
+                      <p className="about">{item.aboutMe}</p>
+                      <div className="link-btn-card">
+                        <Link to="#">Открыть</Link>
+                        <Link to="#">В избранное</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 MainContainer.propTypes = {
